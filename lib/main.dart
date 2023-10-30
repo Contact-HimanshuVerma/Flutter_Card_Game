c6810f4f14a862a1934335cb328ae267aad18353
Himanshu Verma <vermahimanshu232@gmail.com>
2023-10-30 23:54:06 +0530

    list

--- lib/main.dart
+++ lib/main.dart
@@ -5,55 +5,79 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    const appTitle = "Card Game";
+    const appTitle = "To-Do List";
 
     return MaterialApp(
-        title: appTitle,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Center(child: const Text(appTitle)),
-            ),
+      title: appTitle,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: Center(child: const Text(appTitle)),
           ),
-          body: Texts(),
-        ));
+        ),
+        body: TodoList(),
+      ),
+    );
   }
 }
 
-class Texts extends StatelessWidget {
-  Texts({super.key});
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
   TextEditingController _nameController = TextEditingController();
+  List<String> todos = [];
+
+  void addToDo() {
+    setState(() {
+      String todoText = _nameController.text.trim();
+      if (todoText.isNotEmpty) {
+        todos.add(todoText);
+        _nameController.clear();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 50),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: <Widget>[
           TextField(
             decoration: InputDecoration(
               border: OutlineInputBorder(),
-              hintText: 'Enter card options',
+              hintText: 'Enter a to-do item',
             ),
             controller: _nameController,
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('+ Add'),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: addToDo,
+            child: Text('Add'),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(todos[index]),
+                );
+              },
             ),
-          )
+          ),
         ],
       ),
     );
